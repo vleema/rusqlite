02ee723c4b9ace302c18ba2d5c6f6a7218369ae3
Vinicius Sampaio <vldsampaio@pm.me>
2025-11-21 17:35:17 -0300

    page: add module documentation

--- src/page.rs
+++ src/page.rs
@@ -1,5 +1,98 @@
+//! Implements datatypes and functionalities for B-tree pages.
+//!
+//! A b-tree page is either an interior page or a leaf page. A leaf page contains keys and in the case of a
+//! table b-tree each key has associated data. An interior page contains K keys together with K+1 pointers to
+//! child b-tree pages. A "pointer" in an interior b-tree page is just the 32-bit unsigned integer page number
+//! of the child page.
+//!
+//! Each entry in a table b-tree consists of a 64-bit signed integer key and up to 2147483647 bytes of
+//! arbitrary data. (The key of a table b-tree corresponds to the rowid of the SQL table that the b-tree
+//! implements.) Interior table b-trees hold only keys and pointers to children. All data is contained in the
+//! table b-tree leaves.
+//!
+//! When the size of payload for a cell exceeds a certain threshold (to be defined later) then only the first
+//! few bytes of the payload are stored on the b-tree page and the balance is stored in a linked list of
+//! content overflow pages.
+//!
+//! A b-tree page is divided into regions in the following order:
+//!
+//! 1. The 100-byte database file header (found on page 1 only)
+//! 2. The 8 or 12 byte b-tree page header
+//! 3. The cell pointer array
+//! 4. Unallocated space
+//! 5. The cell content area
+//! 6. The reserved region
+//!
+//! The 100-byte database file header is found only on page 1, which is always a table b-tree page. All other
+//! b-tree pages in the database file omit this 100-byte header.
+//!
+//! The b-tree page header is 8 bytes in size for leaf pages and 12 bytes for interior pages. All multibyte
+//! values in the page header are big-endian. The b-tree page header is composed of the following fields:
+//!
+//! | Offset | Size | Description                                                                                                                          |
+//! |--------|------|--------------------------------------------------------------------------------------------------------------------------------------|
+//! | 0      | 1    | The one-byte flag at offset 0 indicating the b-tree page type:                                                                       |
+//! |        |      |   • 2 (0x02): interior index b-tree page                                                                                             |
+//! |        |      |   • 5 (0x05): interior table b-tree page                                                                                             |
+//! |        |      |   • 10 (0x0a): leaf index b-tree page                                                                                                |
+//! |        |      |   • 13 (0x0d): leaf table b-tree page                                                                                                |
+//! |        |      | Any other value is an error.                                                                                                         |
+//! | 1      | 2    | The two-byte integer at offset 1 gives the start of the first freeblock on the page, or zero if none.                                |
+//! | 3      | 2    | The two-byte integer at offset 3 gives the number of cells on the page.                                                              |
+//! | 5      | 2    | The two-byte integer at offset 5 designates the start of the cell content area. Zero is interpreted as 65536.                        |
+//! | 7      | 1    | The one-byte integer at offset 7 gives the number of fragmented free bytes within the cell content area.                             |
+//! | 8      | 4    | The four-byte page number at offset 8 is the right-most pointer. Present only in interior b-tree pages; omitted for all other pages. |
+//!
+//! The cell pointer array of a b-tree page immediately follows the b-tree page header. Let K be the number of
+//! cells on the btree. The cell pointer array consists of K 2-byte integer offsets to the cell contents. The
+//! cell pointers are arranged in key order with left-most cell (the cell with the smallest key) first and the
+//! right-most cell (the cell with the largest key) last. Cell content is stored in the cell content region
+//! of the b-tree page.
+//!
+//! Cells in a page B-tree follows the format:
+//!
+//! | Datatype   | Table Leaf (0x0d) | Index Leaf (0x05) | Index Leaf (0x0a) | Index Interior (0x02)| Description     |
+//! |:-----------|:-----------------:|:-----------------:|:-----------------:|:--------------------:|:----------------|
+//! | 4-byte int |                   |                   | x                 | x                    | Left child page |
+//! | varint     | x                 |                   | x                 | x                    | Payload size    |
+//! | varint     | x                 | x                 |                   |                      | Rowid           |
+//! | bytes      | x                 |                   | x                 | x                    | Payload         |
+//! | 4-byte int | x                 |                   | x                 | x                    | Overflow page   |
+//!
+//! Define the "payload" of a cell to be the arbitrary length section of the cell. For an index b-tree, the
+//! key is always arbitrary in length and hence the payload is the key. There are no arbitrary length elements
+//! in the cells of interior table b-tree pages and so those cells have no payload. Table b-tree leaf pages
+//! contain arbitrary length content and so for cells on those pages the payload is the content.
+//!
+//! The payload is structured in something called the Record Format.
+//!
+//! A record contains a header and a body, in that order. The header begins with a single varint which
+//! determines the total number of bytes in the header. The varint value is the size of the header in bytes
+//! including the size varint itself. Following the size varint are one or more additional varints, one per
+//! column. These additional varints are called "serial type" numbers and determine the datatype of each
+//! column, according to the following chart:
+//!
+//! | Serial Type    | Content Size | Meaning                                                                                                |
+//! |----------------|--------------|--------------------------------------------------------------------------------------------------------|
+//! | 0              | 0            | Value is a NULL.                                                                                       |
+//! | 1              | 1            | Value is an 8-bit twos-complement integer.                                                             |
+//! | 2              | 2            | Value is a big-endian 16-bit twos-complement integer.                                                  |
+//! | 3              | 3            | Value is a big-endian 24-bit twos-complement integer.                                                  |
+//! | 4              | 4            | Value is a big-endian 32-bit twos-complement integer.                                                  |
+//! | 5              | 6            | Value is a big-endian 48-bit twos-complement integer.                                                  |
+//! | 6              | 8            | Value is a big-endian 64-bit twos-complement integer.                                                  |
+//! | 7              | 8            | Value is a big-endian IEEE 754-2008 64-bit floating point number.                                      |
+//! | 8              | 0            | Value is the integer 0. (Only available for schema format 4 and higher.)                               |
+//! | 9              | 0            | Value is the integer 1. (Only available for schema format 4 and higher.)                               |
+//! | 10,11          | variable     | Reserved for internal use. These serial type codes will never appear in a well-formed database file.   |
+//! | ≥ 12 and even  | (N-12)/2     | Value is a BLOB that is (N-12)/2 bytes in length.                                                      |
+//! | ≥ 13 and odd   | (N-13)/2     | Value is a string in the text encoding and (N-13)/2 bytes in length. The nul terminator is not stored. |
+//!
+//! Taken from <https://sqlite.org/fileformat2.html>, more information there.
+use crate::read_varint;
 use anyhow::{Result, bail};
 
+/// Represents a b-tree page table type, either a index table or a regular table.
 #[derive(Debug, Clone, Copy)]
 pub enum TableType {
     Index,
@@ -8,6 +101,15 @@ pub enum TableType {
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy)]
+/// Reprents a SQLite page type, either a interior b-tree page or leaf b-tree page.
+///
+/// A page type is encoded as:
+///  - 0x02 for interior index b-tree page
+///  - 0x05 for interior table b-tree page
+///  - 0x0a for leaf index b-tree page
+///  - 0x0d for leaf table b-tree page
+///
+/// Implements `From<u8>`, will panic if passed `u8` is invalid.
 pub enum PageType {
     Interior(TableType),
     Leaf(TableType),
@@ -28,6 +130,7 @@ impl From<u8> for PageType {
     }
 }
 
+/// Represents a SQLite page header.
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy)]
 pub struct PageHeader {
@@ -40,6 +143,7 @@ pub struct PageHeader {
 }
 
 impl PageHeader {
+    /// Creates a new header
     pub fn new(bytes: &[u8]) -> Result<Self> {
         if bytes.is_empty() {
             bail!("invalid page header")
@@ -61,6 +165,10 @@ impl PageHeader {
         })
     }
 }
+
+/// Represents the serial type encoding.
+///
+/// Implements `From<u64>`.
 #[allow(dead_code)]
 pub enum SerialType {
     Null,
@@ -99,6 +207,7 @@ impl From<u64> for SerialType {
     }
 }
 
+/// Represents the [sqlite_schema](https://sqlite.org/schematab.html) cell structure.
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy)]
 pub struct SchemaCell<'a> {
