74fc41482b336e20f3b90405548e22a7895b64e5
Levi Gabriel de Araújo Marques <levigamarques@gmail.com>
2025-11-22 17:46:19 -0300

    incluindo os parsers na função novo_comando e as otimizando

--- src/main.rs
+++ src/main.rs
@@ -3,66 +3,45 @@ use nom::bytes::complete::*;
 use nom::sequence::delimited;
 use nom::Parser;
 use nom::{IResult, error::Error};
+use nom::branch::alt;
+use nom::sequence::preceded;
 
+#[derive(Debug)]
 struct Comando{
   digitado: String,
-  colunas: &str,
-  arquivo: &str,
-  predicados: todo!()
-}
-
-struct SELECT<'a>{
-  colunas: &'a str
-}
-
-struct FROM<'a>{
-  arquivo: &'a str
-}
-
-struct WHERE{
+  colunas: String,
+  arquivo: String,
   predicados: bool
-  //tô em duvida de como fazer esse
-  //seria o resultado dos predicados? codei como se fosse
 }
 
-impl Default for WHERE {
-    fn default() -> Self {
-        WHERE {
-            predicados: true
-        }
-    }
-}
+fn novo_comando(input: String) -> Comando{
 
-fn parse_select(input: &str) -> SELECT{
+  //essa linha tenta pegar oque não for " " depois de "Select ", retornando Ok((resto, resultado)) ou Err(algo)
+  let resultado: IResult<&str, &str, Error<&str>> = preceded(tag_no_case("SELECT "), is_not(" ")).parse(&input);
+  //essa extrai uma tupla do resultado, mandando mensagem caso seja Err
+  let parse_select = resultado.expect("erro no parse do select");
+  //pega o resultado, nesse caso as colunas
+  let colunas = String::from(parse_select.1);
+
+  //essa linha tenta pegar oque não for " " depois de " FROM " do resto do parsing anteiror, retornando Ok((resto, resultado)) ou Err(algo)
+  let resultado: IResult<&str, &str, Error<&str>> = preceded(tag_no_case(" FROM "), is_not(" ")).parse(parse_select.0);
+  //essa extrai uma tupla do resultado, mandando mensagem caso seja Err
+  let parse_from = resultado.expect("erro no parse do from");
+  //pega o resultado, nesse caso o arquivo
+  let arquivo = String::from(parse_from.1);
+
+  //falta fazer o parse do WHERE
   
-  //associei o resultado do expect para outra variavel
-  //fiz isso para que o tag_no_case pudesse inferir o tipo de Error, uma solução rapida
-  let resultado: IResult<&str, &str, Error<&str>> = delimited(tag_no_case("SELECT "), is_not(" "), char(' ')).parse(input);
-  let outro = resultado.expect("erro");
-  let selecionar = SELECT{
-    colunas: outro.1
-  };
-  selecionar
-
+  Comando{
+    digitado: input.clone(),
+    colunas: colunas,
+    arquivo: arquivo,
+    predicados: true
+  }
 }
 
-fn parse_from(input: &str) -> FROM{
-  
-  let resultado: IResult<&str, &str, Error<&str>> = delimited(tag_no_case("FROM "), is_not(" "), char(' ')).parse(input);
-  let outro = resultado.expect("erro");
-  let de = FROM{
-    arquivo: outro.1
-  };
-  de
-
-}
-
-fn parse_where(input: &str) -> WHERE{
-  
-  //seria aqui iriamos separar os predicados e ver o resultado?
-  todo!();
-}
 
 fn main() {
-  println!("Hello, world!");
+  //teste
+  println!("{:?}", novo_comando(String::from("Select coluna from arquivo where")));
 }
