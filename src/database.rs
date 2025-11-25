5f9d950b91151f7fd539baaf13aeb57999a033ee
Vinicius Sampaio <vldsampaio@pm.me>
2025-11-25 16:07:36 -0300

    Refactor page.rs into database.rs and record.rs
    This refactor simplifies page parsing and uses memmap for simplifying
    database file reading.

---
+++ src/database.rs
@@ -0,0 +1,87 @@
+use anyhow::Result;
+use memmap::Mmap;
+use memmap::MmapOptions;
+use std::fs::File;
+
+pub type PageNumber = u32;
+
+pub struct Database {
+    pub mmap: Mmap,
+    pub page_size: u16,
+    pub page_count: PageNumber,
+}
+
+impl Database {
+    pub fn new(file: &File) -> Result<Self> {
+        // SAFETY: In Wedson we trust 🙏
+        let mmap = unsafe { MmapOptions::new().map(file)? };
+        let page_size = u16::from_be_bytes([mmap[16], mmap[17]]);
+        let page_count = PageNumber::from_be_bytes([mmap[28], mmap[29], mmap[30], mmap[31]]);
+        Ok(Self {
+            mmap,
+            page_size,
+            page_count,
+        })
+    }
+
+    pub fn get_page(&self, page_number: PageNumber) -> Page<'_> {
+        if page_number > self.page_count {
+            panic!("invalid page number")
+        }
+        Page::parse(&self.mmap, page_number, self.page_size)
+    }
+}
+
+#[allow(dead_code)]
+pub enum Page<'a> {
+    Interior {
+        page_number: PageNumber,
+        cell_area_offset: u16,
+        cell_count: u32,
+        cell_offset_list: &'a [u8], // A offset is represent by two sequential bytes in this list.
+        right_child: u32,
+    },
+    Leaf {
+        page_number: PageNumber,
+        cell_area_offset: u16,
+        cell_count: u32,
+        cell_offset_list: &'a [u8],
+    },
+}
+
+impl<'a> Page<'a> {
+    fn parse(mmap: &'a Mmap, page_number: PageNumber, page_size: u16) -> Self {
+        if page_size == 0 || page_number == 0 {
+            panic!("invalid page size or page number")
+        }
+
+        let offset = if page_number == 1 {
+            100
+        } else {
+            ((page_number - 1) * page_size as u32) as usize
+        };
+
+        let page = &mmap[offset..offset + page_size as usize];
+        let page_type = page[0];
+        let cell_count = u16::from_be_bytes([page[3], page[4]]) as u32;
+        let cell_area_offset = u16::from_be_bytes([page[5], page[6]]);
+
+        // Assuming that we have no index tables.
+        match page_type {
+            0x05 => Self::Interior {
+                page_number,
+                cell_area_offset,
+                cell_count,
+                cell_offset_list: &page[12..12 + cell_count as usize * 2],
+                right_child: u32::from_be_bytes([page[8], page[9], page[10], page[11]]),
+            },
+            0x0d => Self::Leaf {
+                page_number,
+                cell_area_offset,
+                cell_count,
+                cell_offset_list: &page[8..8 + cell_count as usize * 2],
+            },
+            _ => panic!("corrupt database"),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use anyhow::{Result, bail};
-use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::Read;
-use std::sync::atomic::AtomicU16;
-use std::sync::atomic::Ordering;
-
-mod page;
+mod database;
+mod record;
 mod varint;
 
-use page::{PageHeader, PageType, SchemaCell};
-use varint::{read_varint, read_varint_from_offset};
+use std::fs::File;
 
-static PAGE_SIZE: AtomicU16 = AtomicU16::new(4096);
+use anyhow::Ok;
+use anyhow::{Result, bail};
+
+use database::Database;
+use database::Page;
+use record::SchemaCell;
+
+use varint::read_varint;
 
 fn main() -> Result<()> {
     let args = std::env::args().collect::<Vec<_>>();
@@ -21,81 +21,39 @@ fn main() -> Result<()> {
         _ => {}
     }
 
+    let file = File::open(&args[1])?;
+    let db = Database::new(&file)?;
+
     let command = &args[2];
     match command.as_str() {
         ".dbinfo" => {
-            let mut file = File::open(&args[1])?;
-            let mut header = [0; 100];
-            file.read_exact(&mut header)?;
-
-            // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            PAGE_SIZE.store(
-                u16::from_be_bytes([header[16], header[17]]),
-                Ordering::Relaxed,
-            );
-
-            println!("database page size: {}", PAGE_SIZE.load(Ordering::Relaxed));
-
-            let mut sqlite_schema_header = [0; 12];
-            file.read_exact(&mut sqlite_schema_header)?;
-            let number_of_tables =
-                u16::from_be_bytes([sqlite_schema_header[3], sqlite_schema_header[4]]);
-
-            println!("number of tables: {number_of_tables}")
-        }
-        ".tables" => {
-            // Read the database header.
-            let mut file = BufReader::new(File::open(&args[1])?);
-            let mut db_header = [0; 100];
-            PAGE_SIZE.store(
-                u16::from_be_bytes([db_header[16], db_header[17]]),
-                Ordering::Relaxed,
-            );
-            file.read_exact(&mut db_header)?;
-
-            // Read first page header.
-            //
-            // The header could occuppy a total of 12 bytes if it's a interior node of the tree. 8
-            // if it's a leaf node.
-            let mut header_bytes = [0; 12];
-            file.read_exact(&mut header_bytes)?;
-            let page_header = PageHeader::new(header_bytes.as_slice())?;
-            if matches!(page_header.ty, PageType::Leaf(..)) {
-                file.seek_relative(-4)?;
+            println!("database page size: {}", db.page_size);
+            if let Page::Leaf { cell_count, .. } = db.get_page(1) {
+                println!("number of tables: {}", cell_count);
+            } else {
+                todo!()
             }
+        }
+        Cmd::Tables => {
+            if let Page::Leaf {
+                cell_offset_list, ..
+            } = db.get_page(1)
+            {
+                for bs in cell_offset_list.chunks_exact(2) {
+                    let offset = u16::from_be_bytes([bs[0], bs[1]]);
 
-            // Read cell pointer array.
-            //
-            // The cells are ordered by key size. Lefmost cell has the key with smallest number,
-            // rightmost with the bigger number.
-            let mut cell_ptr_arr_bytes = vec![0; page_header.number_of_cells as usize * 2];
-            file.read_exact(&mut cell_ptr_arr_bytes)?;
+                    let mut cursor = &db.mmap[offset as usize..];
+                    let (payload_size, payload_int_size) = read_varint(&mut cursor);
+                    let (_, _) = read_varint(&mut cursor); // rowid.
+                    let payload_body_size = payload_size - (payload_int_size) as i64;
+                    let payload_bytes = &cursor[..payload_body_size as usize];
 
-            // HACK: Avoid reallocation?
-            let cell_ptr_arr = cell_ptr_arr_bytes
-                .chunks_exact(2)
-                .map(|offset| u16::from_be_bytes([offset[0], offset[1]]))
-                .collect::<Vec<_>>();
-
-            // Visit cells and print the table name.
-            //
-            // Assuming that sqlite_shchema is always a leaf table.
-            if matches!(page_header.ty, PageType::Leaf(..)) {
-                for offset in cell_ptr_arr {
-                    // Page 1, it's safe to directly go to the offset.
-                    let payload_size = read_varint_from_offset(&mut file, offset as u64)? as usize;
-
-                    // Assuming that's always a rowid table.
-                    let _rowid = read_varint(&mut file)?;
-
-                    // First we read the payload.
-                    let mut payload_bytes = vec![0; payload_size];
-                    file.read_exact(&mut payload_bytes)?;
-
-                    let schema_cell = SchemaCell::new(&payload_bytes)?;
+                    let schema_cell = SchemaCell::new(payload_bytes);
                     print!("{} ", schema_cell.tbl_name)
                 }
-                println!()
+                println!();
+            } else {
+                todo!()
             }
         }
         _ => bail!("Missing or invalid command passed: {}", command),
