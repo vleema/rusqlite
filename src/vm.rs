ef445310a68f319ce3da298b0b161a50d19eb73f
Vinicius Sampaio <vldsampaio@pm.me>
2025-11-30 19:43:36 -0300

    vm: add todolist for where expression and swap arguments of ParsedEntry

--- src/vm.rs
+++ src/vm.rs
@@ -15,13 +15,17 @@ use crate::Schema;
 use crate::SerialType;
 use crate::read_varint;
 
-type ParsedEntry<'a> = Vec<(Value<'a>, &'a str)>;
+type ParsedEntry<'a> = Vec<(&'a str, Value<'a>)>;
 
 pub fn handle_query(db: &Database, query: &str) -> Result<()> {
     let select = sql::select(query)?;
     let schema = get_tbl_schema(db, select.table)?;
     let ct = sql::create_table(&schema.sql).expect("corrupt table");
-    // TODO: Implement where clause.
+    // Verificar se a expressão é válida comparando tipos e colunas com o que existe em `ct`
+    // Se for
+    //      Aplicar uma função que aceita ou não aceita uma entrada da tabela com base na expressão where
+    // Caso contrário
+    //      Erro (não é pânico)
     match select.columns {
         SelectColStmt::List(list) => {
             let selected = match list {
@@ -32,7 +36,8 @@ pub fn handle_query(db: &Database, query: &str) -> Result<()> {
                 SelectCols::All => &ct.columns,
             };
             for e in db.get_page(schema.rootpage).entries() {
-                print_row(selected, parse_entry(&ct, &e), Some);
+                // Passar função que vocês implementaram aqui ↓
+                print_row(selected, parse_entry(&ct, &e), |_| true);
             }
         }
         SelectColStmt::Count(_) => {
@@ -48,7 +53,7 @@ pub fn handle_query(db: &Database, query: &str) -> Result<()> {
                 let cloned = entry.clone();
                 let parsed_row = parse_entry(&ct, &cloned);
 
-                for (val, c) in parsed_row {
+                for (c, val) in parsed_row {
                     if c == col {
                         count += 1;
                         match val {
@@ -82,25 +87,21 @@ fn parse_entry<'a>(ct: &'a CreateTable<'a>, entry: &'a Entry) -> ParsedEntry<'a>
         .zip(&ct.columns)
         .map(|(v, c)| {
             if c.primary_key && c.sql_type == SqlType::Integer {
-                (Value::Int(entry.key), c.name)
+                (c.name, Value::Int(entry.key))
             } else {
-                (v, c.name)
+                (c.name, v)
             }
         })
         .collect()
 }
 
-fn print_row<'a>(
-    selected: &[ColumnDef],
-    pe: ParsedEntry<'a>,
-    wrap: impl Fn(ParsedEntry<'a>) -> Option<ParsedEntry<'a>>,
-) {
-    let Some(result) = wrap(pe) else {
+fn print_row<'a>(selected: &[ColumnDef], pe: ParsedEntry<'a>, p: impl FnOnce(&ParsedEntry<'a>) -> bool) {
+    if !p(&pe) {
         return;
     };
     let mut iter = selected
         .iter()
-        .flat_map(|s| result.iter().find_map(|(v, c)| (s.name == *c).then_some(v)));
+        .flat_map(|s| pe.iter().find_map(|(c, v)| (s.name == *c).then_some(v)));
     iter.next().inspect(|v| print!("{v}"));
     for v in iter {
         print!("|{v}")
